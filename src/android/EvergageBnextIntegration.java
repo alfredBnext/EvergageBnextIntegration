3e6fd1b851ed75ba9132a6ad37076fdf29a98981
Jose Alfredo Rodriguez Jaquez <jrodriguez@bnext.mx>
2022-06-22 12:51:19 -0500

    Event Start

--- src/android/EvergageBnextIntegration.java
+++ src/android/EvergageBnextIntegration.java
@@ -7,10 +7,20 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-/**
- * This class echoes a string called from JavaScript.
- */
+import com.evergage.android.ClientConfiguration;
+import com.evergage.android.Evergage;
+import com.evergage.android.LogLevel;
+import com.evergage.android.Screen;
+
 public class EvergageBnextIntegration extends CordovaPlugin {
+    Evergage evergage;
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        Evergage.initialize(this.cordova.getActivity().getApplicationContext());
+        Evergage evergage = Evergage.getInstance();
+    }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -19,6 +29,15 @@ public class EvergageBnextIntegration extends CordovaPlugin {
             this.coolMethod(message, callbackContext);
             return true;
         }
+
+        if (action.equals("start")) {
+            String account = args.getString(0);
+            String dataset = args.getString(1);
+            boolean usePushNotification = args.getBoolean(2);
+            this.start(account, dataset, usePushNotification);
+            return true;
+        }
+
         return false;
     }
 
@@ -27,6 +46,19 @@ public class EvergageBnextIntegration extends CordovaPlugin {
             callbackContext.success(message);
         } else {
             callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+    private void start(String account, String dateset, boolean usePushNotification, CallbackContext callbackContext){
+        try{
+            evergage.start(new ClientConfiguration.Builder()
+                .account(account)
+                .dataset(dateset)
+                .usePushNotifications(usePushNotification)
+                .build());
+            callbackContext.success(message);
+        }catch (Exception e){
+            callbackContext.error(e.toString());
         }
     }
 }
--- www/EvergageBnextIntegration.js
+++ www/EvergageBnextIntegration.js
@@ -1,7 +1,12 @@
 var exec = require('cordova/exec');
 
 module.exports = {
+    
     coolMethod: function (arg0, success, error) {
         exec(success, error, 'EvergageBnextIntegration', 'coolMethod', [arg0]);
+    },
+
+    start: function(account, dataset, usePushNotification, success, error){
+        exec(success, error,'EvergageBnextIntegration', 'start', [account, dataset, usePushNotification]);
     }
 };
