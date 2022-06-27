a2c3be5d8052a6ec5f6a570e9395c925e0d586f5
Jose Alfredo Rodriguez Jaquez <jrodriguez@bnext.mx>
2022-06-27 16:20:56 -0500

    se agrego dependence

--- src/android/MainActivity.java
+++ src/android/MainActivity.java
@@ -1,31 +1,3 @@
-
-Skip to content
-Pulls
-Issues
-Marketplace
-Explore
-@AlfredRds
-dpa99c /
-cordova-plugin-popupbridge
-Public
-
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-
-    Insights
-
-cordova-plugin-popupbridge/src/android/MainActivity.java /
-@dpa99c
-dpa99c Fix activity
-Latest commit 0a54803 on 17 Jul 2017
-History
-1 contributor
-50 lines (39 sloc) 1.67 KB
 /*
        Licensed to the Apache Software Foundation (ASF) under one
        or more contributor license agreements.  See the NOTICE file
@@ -34,7 +6,9 @@ History
        to you under the Apache License, Version 2.0 (the
        "License"); you may not use this file except in compliance
        with the License.  You may obtain a copy of the License at
+
          http://www.apache.org/licenses/LICENSE-2.0
+
        Unless required by applicable law or agreed to in writing,
        software distributed under the License is distributed on an
        "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -71,4 +45,3 @@ public class MainActivity extends CordovaActivity
 
     }
 }
-
