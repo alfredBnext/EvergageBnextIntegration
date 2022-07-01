440ded7e02db74578a7b7f108a4b2779f011020b
Jose Alfredo Rodriguez Jaquez <jrodriguez@bnext.mx>
2022-07-01 12:55:17 -0500

    Inicio para poder manejar una venta en plugin

---
+++ src/android/Models/ListSaleLine.java
@@ -0,0 +1,15 @@
+package mx.bnext.EvergageBnextIntegration.Models;
+
+import java.util.List;
+
+public class ListSaleLine {
+    List<SaleLine> list;
+
+    public List<SaleLine> getList() {
+        return list;
+    }
+
+    public void setList(List<SaleLine> list) {
+        this.list = list;
+    }
+}
